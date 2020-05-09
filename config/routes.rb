1f5fcd717d73f356723a0fab7a1bc95e24d0afa0
Sai-Yan-Naing <58454014+Sai-Yan-Naing@users.noreply.github.com>
2020-05-09 16:06:25 +0630

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'users/index'
   get 'home/index'
+  get '/signup', to: 'users#new'
   delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'reports#index'
